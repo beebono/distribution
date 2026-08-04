74ee3b5f2cb956f8c19bc112555f76973477efdb
Noxwell <beebono@yahoo.com>
2026-08-03 21:00:01 -0500

    t618: fix reserved memory

--- projects/ROCKNIX/packages/linux/package.mk
+++ projects/ROCKNIX/packages/linux/package.mk
@@ -32,7 +32,7 @@ case ${DEVICE} in
     PKG_PATCH_DIRS+=" 7.0"
     ;;
   T618)
-    PKG_VERSION="262f000ae3da6160d97b4b6b8833228ea8e93477"
+    PKG_VERSION="66bb4b92846cb47d3168c29c31ee38a4003c9779"
     PKG_URL="https://github.com/beebono/linux-mainline-sprd/archive/${PKG_VERSION}.tar.gz"
     PKG_GIT_CLONE_BRANCH="rg-rotate"
     PKG_PATCH_DIRS+=" 7.0"
